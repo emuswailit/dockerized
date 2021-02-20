3dda7cd89d27efb8c77192c2ebe6b2d20c2fd097
emuswailit <emuswailit@gmail.com>
2021-02-20 08:35:40 +0300

    Static root vs files

--- app/mobipharma_project/settings.py
+++ app/mobipharma_project/settings.py
@@ -305,11 +305,11 @@ AUTH_USER_MODEL = 'users.User'
 
 
 # Remote setting
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles/'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles/'),
+# )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
