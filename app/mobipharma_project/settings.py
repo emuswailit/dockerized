eec004dfaa2cde2c0f6193c47e1a8fb133607932
emuswailit <emuswailit@gmail.com>
2021-02-20 08:44:35 +0300

    Changets to settings

--- app/mobipharma_project/settings.py
+++ app/mobipharma_project/settings.py
@@ -296,16 +296,16 @@ AUTH_USER_MODEL = 'users.User'
 
 
 # Localhost settings
-# if DEBUG:
-#     STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'staticfiles')
-#     ]
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'staticfiles')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Remote setting
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'staticfiles/'),
